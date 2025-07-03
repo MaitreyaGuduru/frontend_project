9aa4b8fb807e39b9764dd849f365eb49a6489959
Maitreya Guduru <maitreyaguduru@Maitreyas-MacBook-Pro.local>
2025-07-03 19:07:54 +0530

    frontend initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ISSGlobe from './components/ISSGlobe';
+import "cesium/Build/Cesium/Widgets/widgets.css";
+
+
+function App() {
+  return (
+    <div className="App">
+      <ISSGlobe />
+    </div>
+  );
+}
+
+export default App;
